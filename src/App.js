23910b7090f750dd65924b31722685efaa777741
sam-levin <sam.ky.levin@gmail.com>
2022-06-19 19:55:11 -0700

    making user form interactive with states

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
